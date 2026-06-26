2ed6942447e463084245e3fca924ba82be418269
Cleversupport <128171807+Cleversupport@users.noreply.github.com>
2026-06-26 15:20:57 -0400

    Enable native GitHub updates and bump to 1.5.16

--- email-templates.php
+++ email-templates.php
@@ -2,20 +2,22 @@
 /**
  * Email Templates
  *
- * @link              https://www.wpexperts.io/
+ * @link              https://github.com/Cleversupport/clever-email-templates
  * @since             2.0
  * @package           Mailtpl
  *
  * @wordpress-plugin
- * Plugin Name:       Email Templates
- * Plugin URI:        http://wordpress.org/plugins/email-templates
- * Description:       Beautify WordPress default emails
- * Version:           1.5.14
+ * Plugin Name:       Clever Email Templates
+ * Plugin URI:        https://github.com/Cleversupport/clever-email-templates
+ * GitHub Plugin URI: https://github.com/Cleversupport/clever-email-templates
+ * Description:       Site Mailer compatible HTML email templates for WordPress.
+ * Version:           1.5.16
  * Requires at least: 4.8
- * Requires PHP:	  7.1
- * Tested up to: 	  7.0
- * Author:            WPExperts.io
- * Author URI:        https://www.wpexperts.io/
+ * Requires PHP:      7.1
+ * Tested up to:      7.0
+ * Author:            WPExperts.io, Clever
+ * Author URI:        https://github.com/Cleversupport
+ * Update URI:        https://github.com/Cleversupport/clever-email-templates
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       email-templates
@@ -26,7 +28,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'MAILTPL_VERSION', '1.5.14' );
+define( 'MAILTPL_VERSION', '1.5.16' );
 define( 'MAILTPL_PLUGIN_FILE', __FILE__ );
 define( 'MAILTPL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MAILTPL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
@@ -36,6 +38,9 @@ define( 'MAILTPL_WOOMAIL_PATH', realpath( plugin_dir_path( __FILE__ ) ) . DIRECT
 require_once MAILTPL_PLUGIN_DIR . 'includes/functions.php';
 require_once MAILTPL_PLUGIN_DIR . 'class-mailtpl-woomail-composer.php';
 require_once MAILTPL_PLUGIN_DIR . 'includes/class-mailtpl-plugin-check.php';
+require_once MAILTPL_PLUGIN_DIR . 'includes/class-mailtpl-github-updater.php';
+
+new Mailtpl_GitHub_Updater( MAILTPL_PLUGIN_FILE, MAILTPL_VERSION );
 
 mailtpl_email_templates();
 
